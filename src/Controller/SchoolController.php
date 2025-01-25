f839711c31efdea973f610cb9f0d86991679b798
Al Zee <z@alz.ee>
2025-01-26 05:43:32 +0800

    nth

--- src/Controller/SchoolController.php
+++ src/Controller/SchoolController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\School;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class SchoolController extends AbstractController
 {
     #[Route('/school', name: 'app_school')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $schools = $entityManager->getRepository(School::class)->findAll();
+
         return $this->render('school/index.html.twig', [
-            'controller_name' => 'SchoolController',
+            'schools' => $schools,
         ]);
     }
 } 
